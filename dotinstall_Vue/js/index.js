8dff15b6ce26b3a40d66f799157636ad4ba6f18f
hirdot <h.ootake.2196s43d@gmail.com>
2018-08-15 17:36:04 +0900

    [add] dotinstallの写経をコミット

---
+++ dotinstall_Vue/js/index.js
@@ -0,0 +1,15 @@
+(function(){
+  'use strict';
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      name: 'Hiroto',
+      todos: [
+        'task 1',
+        'task 2',
+        'task 3'
+      ]
+    }
+  });
+})();
