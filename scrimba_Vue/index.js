d652b3b2f0e70e76806641b789cef05973d2f8a3
hiroto.ootake <otake@piano.or.jp>
2018-08-15 17:55:36 +0900

    [add] Scrimba:for loop

--- scrimba_Vue/index.js
+++ scrimba_Vue/index.js
@@ -2,9 +2,15 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!',
-    seen: true
+    seen: true,
+    todos:[
+      {text: 'Learn JavaScript'},
+      {text: 'Learn Vue'},
+      {text: 'Build something awesome'}
+    ]
   }
 });
 
 app.message = 'i have changed the data!';
 app.seen = false;
+app.todos.push({text: 'new Item'});
