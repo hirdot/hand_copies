b55fdbdfc17082a6ec3e762d4514df957eb1fbf0
hiroto.ootake <otake@piano.or.jp>
2018-08-17 16:28:15 +0900

    [add] Gihyo：第３回　Vue.component Js の記述順を戻す＋data属性の名称を変更

--- gihyo/article_3/1st.js
+++ gihyo/article_3/1st.js
@@ -1,14 +1,14 @@
+Vue.component('apple-list', {
+  props: ['fruitsItem'],
+  template: '<li>{{fruitsItem.name}}</li>'
+});
+
 new Vue({
   el: '#fruits-component',
   data: {
-    fruitsItems: [
+    fruits: [
       {name: '梨'},
       {name: 'イチゴ'}
     ]
   }
-});
-
-Vue.component('apple-list', {
-  props: ['fruitsItem'],
-  template: '<li>{{fruitsItem.name}}</li>'
 });
