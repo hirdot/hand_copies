badf9ae257faf87325196888f70e381aaa56ec0f
hiroto.ootake <otake@piano.or.jp>
2018-08-17 14:33:36 +0900

    [add] Gihyo：step1 Mustache記法（{{ computed }}）. data.name, data.prop はアクセスできる。

---
+++ gihyo/article_2/2nd.js
@@ -0,0 +1,23 @@
+var vm = new Vue({
+  el: '#example',
+  data: {
+    items: [
+      {
+        name: 'みかん',
+        price: 100
+      },
+      {
+        name: 'もも',
+        price: 300
+      },
+      {
+        name: 'いちじく',
+        price: 500
+      },
+      {
+        name: 'メロン',
+        price: 1000
+      }
+    ]
+  }
+});
